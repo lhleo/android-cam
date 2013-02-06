24ed4c4bfd355d2763039088a73ec864c9472789
nunull <timm@ravepunk.de>
2013-02-06 16:29:26 +0100

    Testing if this will recognize changing single words.

--- src/test/Test.java
+++ src/test/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Just some random shit.");
-		System.out.println("Another line...");
+		System.out.println("More lines...");
 
 	}
 
