ad366a89b0b06de2a7f579de2af7b6660e59c38d
nunull <timm@ravepunk.de>
2013-02-20 14:13:16 +0100

    test

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	 * @param args ignored
 	 */
 	public static void main(String[] args) {
-		System.out.println("Just some random shit.");
+		System.out.println("Just some random shit...");
 	}
 
 }
